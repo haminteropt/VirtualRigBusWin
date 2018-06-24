f9ab0a1bf5cdc0b29e5f92d227459f5eeafc44f7
DELENN\wa1go <darryl@hambus.org>
2018-06-24 17:12:46 -0500

    initial commit

---
+++ VirtualRigBusWin/VirtualRigBusWin/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualRigBusWin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
