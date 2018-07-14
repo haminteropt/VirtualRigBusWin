01415cfa2656b2c86996aebf8f60731d3e62c4e9
DELENN\wa1go <darryl@hambus.org>
2018-07-13 20:19:29 -0500

    url setup

--- VirtualRigBusWin/VirtualKenwoodBusWin/Program.cs
+++ VirtualRigBusWin/VirtualKenwoodBusWin/Program.cs
@@ -20,8 +20,8 @@ namespace VirtualKenwoodBusWin
         {
 
             int httpPort = IpPorts.TcpPort;
+            //var url = string.Format("http://+:{0}/", httpPort);
             var url = string.Format("http://localhost:{0}", httpPort);
-
             var comPort = "com20";
             var udpServer = UdpServer.GetInstance();
             var reportingThread = ReportingThread.GetInstance();
