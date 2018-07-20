5668b56f183957ccab8db896f0f3766aa7ded126
DELENN\wa1go <darryl@hambus.org>
2018-07-19 21:02:55 -0500

    Getting ready to start on client

--- VirtualRigBusWin/VirtualRigBusWin/ngVirtualBus/src/app/app.component.ts
+++ VirtualRigBusWin/VirtualRigBusWin/ngVirtualBus/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DirService, UdpCmdPacket } from './services/DirService.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'app';
+  public serviceList: Array<UdpCmdPacket> = new Array<UdpCmdPacket>();
+  constructor(private dirSrv: DirService) { }
+  ngOnInit() {
+    this.dirSrv.getList().subscribe(val => {
+      if (!val || val.length === 0) return;
+      this.serviceList = val;
+      console.log(val);
+    });
+  }
+
 }
