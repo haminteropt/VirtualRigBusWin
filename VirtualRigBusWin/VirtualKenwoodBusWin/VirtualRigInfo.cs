5e219b3290a692511e74ae9006837f8d966c284f
DELENN\wa1go <darryl@hambus.org>
2018-07-29 12:03:42 -0500

    will start up as many VRigs as defined.

--- VirtualRigBusWin/VirtualKenwoodBusWin/VirtualRigInfo.cs
+++ VirtualRigBusWin/VirtualKenwoodBusWin/VirtualRigInfo.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.IO.Ports;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -26,11 +27,41 @@ namespace VirtualKenwoodBusWin
         public VirtualBusConf GetVirtualRigConfig(string commPort)
         {
             var virtBusConf = new VirtualBusConf();
+            virtBusConf.Host = Dns.GetHostName();
             var commPortConf = new CommPortConf();
 
             // todo replace with call to db
+            commPortConf.DisplayName = "N3FJP Logger";
             commPortConf.BaudRate = 57600;
             commPortConf.PortName = "com20";
+            commPortConf.Parity = Parity.None.ToString();
+            commPortConf.DataBits = 8;
+            commPortConf.Handshake = "none";
+            commPortConf.StopBits = StopBits.One.ToString();
+            commPortConf.ReadTimeout = 5000;
+            commPortConf.WriteTimeout = 500;
+            virtBusConf.CommPorts.Add(commPortConf);
+
+            commPortConf = new CommPortConf();
+
+            // todo replace with call to db
+            commPortConf.DisplayName = "ACLog Logger";
+            commPortConf.BaudRate = 57600;
+            commPortConf.PortName = "com19";
+            commPortConf.Parity = Parity.None.ToString();
+            commPortConf.DataBits = 8;
+            commPortConf.Handshake = "none";
+            commPortConf.StopBits = StopBits.One.ToString();
+            commPortConf.ReadTimeout = 5000;
+            commPortConf.WriteTimeout = 500;
+            virtBusConf.CommPorts.Add(commPortConf);
+
+            commPortConf = new CommPortConf();
+
+            // todo replace with call to db
+            commPortConf.DisplayName = "xyz Logger";
+            commPortConf.BaudRate = 57600;
+            commPortConf.PortName = "com18";
             commPortConf.Parity = Parity.None.ToString();
             commPortConf.DataBits = 8;
             commPortConf.Handshake = "none";
